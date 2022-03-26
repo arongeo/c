7ab100285664c61c54e90c13a6350eee9dbee02a
arongeo <58132897+arongeo@users.noreply.github.com>
2022-03-26 11:18:24 +0000

    added project euler solves

---
+++ projecteuler/pep1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+unsigned int read_input() {
+  int input;
+  scanf("%u", &input);
+  return input;
+}
+
+unsigned int solve_pep1(int input) {
+  unsigned int result = 0;
+  for (int i = 0; i<input; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      result += i;
+    }
+  }
+  printf("%u\n", result);
+  return 0;
+}
+
+int main() {
+  return solve_pep1(read_input());
+}
---
+++ projecteuler/pep3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+unsigned int read_input() {
+  unsigned int input;
+  scanf("%u", &input);
+  return input;
+}
+
+int solve_pep3(unsigned int input) {
+  unsigned int factors[100];
+  unsigned int factors_multiplied;
+  for (int a = 0; a<100; a++) {
+   factors[a] = 1; 
+  }
+  for (int i = 0; i<100; i++) {
+    factors_multiplied = factors_multiplied * factors[i];
+  }
+  while (input != factors_multiplied) {
+    
+  }
+}
+
+int main() {
+  return solve_pep3(read_input());
+}
